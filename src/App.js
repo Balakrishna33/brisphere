db657b28eaf76d5483c7f872db35c55e1481e1df
Balakrishna33 <balakrishakodadhala33@gmail.com>
2024-11-11 15:43:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <HomePage />
+      </main>
+      <Footer />
+    </div>
+  );
+    
+}
+
+export default App;
